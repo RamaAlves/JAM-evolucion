8b82add1f5c102a6f52375ac493442ad906bd47f
manuelxp95 <manuelxp95@gmail.com>
2023-01-09 18:53:24 -0300

    Agregado el movimiento de los lados y arriba

---
+++ Scripts/Jugador/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+const JUMP_VELOCITY = -400.0
+
+
+@export var vel= Vector2(0,1.0)
+
+var mov=Vector2.ZERO
+
+
+func _physics_process(_delta):
+	mov.x= direction_input().x
+	mov.y= -vel.y
+	move_and_collide(mov)
+
+#-----------------> Funciones player
+func direction_input() -> Vector2:
+	var dir= Vector2.ZERO
+	if DataPlayer.can_move:
+		dir = dir_input(dir)
+		if dir.x < 0:
+			pass
+			#carpincho_sprite.flip_h = true
+			#animation.play("walk_side")
+		elif dir.x > 0:
+			pass
+			#carpincho_sprite.flip_h = false
+			#animation.play("walk_side")
+	return dir
+
+func dir_input(direction:Vector2) -> Vector2:
+	if Input.is_action_just_pressed("ui_left"):
+		direction.x = -20
+		#$soundWalk.play()
+		#t_standby.start()
+		#sprite_sit = false
+	elif Input.is_action_just_pressed("ui_right"): 
+		direction.x = 20
+		#$soundWalk.play()
+		#t_standby.start()
+		#sprite_sit = false
+	return direction
